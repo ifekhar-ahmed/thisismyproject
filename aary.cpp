12f7f30670de4babf1c75936b9692fe1a1d1dc61
ifekhar-ahmed <128823059+ifekhar-ahmed@users.noreply.github.com>
2023-10-04 21:23:05 +0600

    Add files via upload

---
+++ aary.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main ()
+{
+
+    int marks [5];
+
+
+ cin >> marks[0];
+  cin >> marks[1];
+   cin >> marks[2];
+    cin >> marks[3];
+     cin >> marks[4];
+
+     cout<<"elements are=" ;
+   for (int i=0; i<5; i++)
+    {
+        cout <<marks [i]<< "  ";
+    }
+    getch();
+}
